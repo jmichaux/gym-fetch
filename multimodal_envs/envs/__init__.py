b5194632460e99fc5582eb7dfb584e02e8628cbb
Jon Michaux <jmichaux@ttic.edu>
2019-04-07 16:16:20 -0700

    adding all env files

---
+++ multimodal_envs/envs/__init__.py
@@ -0,0 +1,5 @@
+from multimodal_envs.envs.fetch_env import FetchEnv
+from multimodal_envs.envs.fetch.reach import FetchReachEnv
+from multimodal_envs.envs.fetch.push import FetchPushEnv
+from multimodal_envs.envs.fetch.slide import FetchSlideEnv
+from multimodal_envs.envs.fetch.pick_and_place import FetchPickAndPlaceEnv
