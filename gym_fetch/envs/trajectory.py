c2413f72967802d7be223e9863ef29173eef12d1
Yongseok Kwon <kwonys@umich.edu>
2021-11-11 00:21:24 -0500

    new branch for trajectory action

---
+++ gym_fetch/envs/trajectory.py
@@ -0,0 +1,43 @@
+import numpy as np
+#try:
+#    import mujoco_py
+#except ImportError as e:
+#    raise error.DependencyNotInstalled("{}. (HINT: you need to install mujoco_py, and also perform the setup instructions here: https://github.com/openai/mujoco-py/.)".format(e))
+
+def gen_traj(ka,q_0,q_dot_0,T_len=1000):
+
+    T = np.linspace(0,1,T_len+1)
+    T_plan = T[:int(T_len/2)]
+    #T_brake = T[int(T_len/2):]
+
+    q_to_peak = q_0.reshape(-1,1) + np.outer(q_dot_0,T_plan) + .5*np.outer(ka,T_plan**2)
+    q_dot_to_peak = q_dot_0.reshape(-1,1) + np.outer(ka,T_plan)
+
+    #q_peak = q_to_peak[:,-1]
+    #q_dot_peak = q_dot_to_peak[:,-1]
+
+           
+    #T_brake = T_brake - T_brake[0]
+    #q_to_stop = q_peak + q_dot_peak.*T_brake + (1/2)*((0 - q_dot_peak)./t_to_stop).*T_brake.^2
+    #q_dot_to_stop = q_dot_peak + ((0 - q_dot_peak)./t_to_stop).*T_brake
+
+    return q_to_peak, q_dot_to_peak, T_plan
+
+def step_traj(self,ka):
+    old_state = self.sim.get_state()
+    traj_qpos, traj_qvel = gen_traj(ka,old_state.qpos,old_state.qvel)
+
+    for i in range(traj_qpos.shape[1]-1):
+        # TO DO: action demension? and qpos demension?
+        # 
+        qpos = traj_qpos[:,i+1]
+        qvel = traj_qvel[:,i+1]
+        new_state = mujoco_py.MjSimState(
+            old_state.time, qpos, qvel, old_state.act, old_state.udd_state
+        )
+        self.sim.set_state(new_state)
+        self.sim.forward()
+        self.render() # might be wrong?
+
+
+if __name__ == '__main__':
