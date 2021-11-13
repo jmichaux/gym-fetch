9f32e48dd676641f5441b4964f7d88748ced1719
Yongseok Kwon <kwonys@umich.edu>
2021-11-12 20:49:20 -0500

    fix test

--- gym_fetch/test.py
+++ gym_fetch/test.py
@@ -3,10 +3,12 @@ import gym
 
 
 #env = gym.make('FetchReach-v1')
-env = gym.make('FetchTraj-v0')
+#env = gym.make('FetchTraj-v0')
+
+env = gym.make('FetchTrajReach-v1')
 
 env.reset()
-for _ in range(1):
+for _ in range(10):
     env.render()
     env.step(env.action_space.sample())
 
